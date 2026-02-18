c562b5b8ac1178da565b2895a2d073c2fe8a2bc0
zerobytes <guiferrolli@gmail.com>
2026-02-18 17:08:37 -0300

    release: v1.1.0

--- types/core/dom/list.d.ts
+++ types/core/dom/list.d.ts
@@ -1,6 +1,7 @@
 export function list(items: any, renderItem: any): ListNode;
 export class ListNode extends Renderable {
     constructor(items: any, renderItem: any);
+    nodeType: string;
     mountInto(parent: any, beforeNode: any): void;
     renderToString(render: any): any;
     #private;
